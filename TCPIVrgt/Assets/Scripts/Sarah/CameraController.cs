09f0575b64cd5809651c5164204c464e25cd6256
sarahmattos <sahmattos13@gmail.com>
2021-12-03 16:30:15 -0300

    testes de parallax e swipe

---
+++ TCPIVrgt/Assets/Scripts/Sarah/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Personagem;
+    
+        void Update()
+        {
+        // Vector3 Ponto = new Vector3(Personagem.transform.position.x, 0, Personagem.transform.position.z );
+        //transform.LookAt(Ponto);
+
+        float x = Personagem.transform.position.x+5f;
+        float z = Personagem.transform.position.z - 10.0f;
+        transform.position = new Vector3(x, 0, z);
+    }
+    
+}
---
+++ TCPIVrgt/Assets/Scripts/Sarah/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight; 
+    public float dir = 1;
+    private Rigidbody2D playerRB;
+    public float maxSwipetime;
+    public float minSwipeDistance;
+
+    private float swipeStartTime;
+    private float swipeEndTime;
+    private float swipeTime;
+
+    private Vector2 startSwipePosition;
+    private Vector2 endSwipePosition;
+    private float swipelength;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        SwipeTest();
+        //transform.Translate(Vector2.right * 10 * Time.deltaTime);
+        playerRB.velocity= new Vector2(dir * speed * Time.deltaTime, playerRB.velocity.y);
+        //playerRB.velocity = Vector2.up * jumpHeight * Time.deltaTime;
+       // if (Input.GetMouseButtonDown(0)) //(Input.GetKeyDown(KeyCode.Space))
+      //  {
+           
+            //transform.Translate(Vector2.up * 100 * Time.deltaTime);
+           // playerRB.velocity = Vector2.up * jumpHeight * Time.deltaTime;
+      //  }
+    }
+
+    void SwipeTest()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                swipeStartTime = Time.time;
+                startSwipePosition = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                swipeEndTime = Time.time;
+                endSwipePosition = touch.position;
+                swipeTime = swipeEndTime - swipeStartTime;
+                swipelength = (endSwipePosition - startSwipePosition).magnitude;
+                if(swipeTime<maxSwipetime&& swipelength > minSwipeDistance)
+                {
+                    swipeControl();
+                }
+            }
+        }
+    }
+
+    void swipeControl()
+    {
+        Vector2 Distance = endSwipePosition - startSwipePosition;
+        float xDistance = Mathf.Abs(Distance.x);
+        float yDistance = Mathf.Abs(Distance.y);
+        if (yDistance > xDistance)
+        {
+            if (Distance.y > 0)
+            {
+                playerRB.velocity = Vector2.up * jumpHeight * Time.deltaTime;
+            }
+            if (Distance.y < 0)
+            {
+                Debug.Log("abaixou");
+            }
+        }
+    }
+
+}
