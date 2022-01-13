1e50a1584201782121089c4901453f9dc884459a
lohannahme <61636785+lohannahme@users.noreply.github.com>
2022-01-13 19:27:07 -0300

    New Menu

---
+++ TCPIVrgt/Assets/Scripts/Hanna/SlimoClick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimoClick : MonoBehaviour
+{
+    [SerializeField] private float _move;
+    [SerializeField] private GameObject _buttons;
+    [SerializeField] private Animator _buttonAnimator;
+
+    private bool _sideLeft = true;
+    private bool _sideRight = false;
+
+    public void ChangeSide()
+
+    {
+        if (_sideLeft)
+        {
+            transform.LeanMoveLocalX(-110, .3f);
+
+            _buttons.transform.LeanMoveLocalX(58, .3f);
+            _buttonAnimator.SetBool("Buttonfade", true);
+
+            _sideLeft = false;
+            _sideRight = true;
+        }
+        else if (_sideRight)
+        {
+            transform.LeanMoveLocalX(0, .3f);
+
+            _buttons.transform.LeanMoveLocalX(0, .3f);
+            _buttonAnimator.SetBool("Buttonfade", false);
+
+            _sideLeft = true;
+            _sideRight = false;
+        }
+    }
+
+
+}
