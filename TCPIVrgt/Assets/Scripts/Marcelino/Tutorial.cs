cccfd8e755d23b3f3668319fd7270c02415dd4a9
MarcelinoABMO <marcelino.jesus@gmail.com>
2022-01-26 22:13:40 -0300

    Ajustes obstaculos; tutorial uma vez.
    Ajustes no tempo de input dos obstaculos; Tutorial roda somente uma vez, tem uma bool no inspector do Tutorial caso queira religar o tutorial.

--- TCPIVrgt/Assets/Scripts/Marcelino/Tutorial.cs
+++ TCPIVrgt/Assets/Scripts/Marcelino/Tutorial.cs
@@ -10,29 +10,40 @@ public class Tutorial : MonoBehaviour
     ETutorialState state = ETutorialState.none;
 
     bool pause = false;
+    int tutorial_off = 0;
+    [SerializeField] bool tutorialAlwaysOn = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (PlayerPrefs.HasKey("tutorial_off") && !tutorialAlwaysOn)
+            tutorial_off = PlayerPrefs.GetInt("tutorial_off");
+
         rede.SetActive(false);
         drone.SetActive(false);
         tiro.SetActive(false);
         coletavel.SetActive(false);
 
-        PlayerController.get.jumpEvent.AddListener(OnRedeHandler);
-        PlayerController.get.jumpEvent.AddListener(OnTiroHandler);
-        PlayerController.get.jumpEvent.AddListener(OnColetavelHandler);
+        if (tutorial_off == 0)
+        {
+            PlayerController.get.jumpEvent.AddListener(OnRedeHandler);
+            PlayerController.get.jumpEvent.AddListener(OnTiroHandler);
+            PlayerController.get.jumpEvent.AddListener(OnColetavelHandler);
 
-        PlayerController.get.crouchEvent.AddListener(OnDroneHandler);
-        PlayerController.get.crouchEvent.AddListener(OnTiroHandler);
-        PlayerController.get.crouchEvent.AddListener(OnColetavelHandler);
+            PlayerController.get.crouchEvent.AddListener(OnDroneHandler);
+            PlayerController.get.crouchEvent.AddListener(OnTiroHandler);
+            PlayerController.get.crouchEvent.AddListener(OnColetavelHandler);
 
-        PlayerController.get.diveEvent.AddListener(OnTiroHandler);
-        PlayerController.get.diveEvent.AddListener(OnColetavelHandler);
+            PlayerController.get.diveEvent.AddListener(OnTiroHandler);
+            PlayerController.get.diveEvent.AddListener(OnColetavelHandler);
+        }
     }
 
     private void TutorialOn(GameObject panel, ETutorialState _state)
     {
+        if (tutorial_off != 0)
+            return;
+
         rede.SetActive(false);
         drone.SetActive(false);
         tiro.SetActive(false);
@@ -67,6 +78,9 @@ public class Tutorial : MonoBehaviour
 
     private void TutorialOff(GameObject panel, ETutorialState _state)
     {
+        if (tutorial_off != 0)
+            return;
+
         pause = false;
         Time.timeScale = 1f;
         panel.SetActive(false);
@@ -92,7 +106,16 @@ public class Tutorial : MonoBehaviour
 
     public void EndTutorial()
     {
-        state = ETutorialState.none;
-        coletavel.SetActive(false);
+        if (tutorial_off != 0)
+            return;
+        //state = ETutorialState.none;
+        TutorialOff(coletavel, ETutorialState.coletavel);//coletavel.SetActive(false);
+        if (tutorialAlwaysOn)
+        {
+            if (PlayerPrefs.HasKey("tutorial_off"))
+                PlayerPrefs.DeleteKey("tutorial_off");
+        }
+        else
+            PlayerPrefs.SetInt("tutorial_off", 1);
     }
 }
